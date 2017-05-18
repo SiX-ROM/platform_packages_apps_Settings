PureNexusProject-Legacy/android_packages_apps_Settings

add activity to launch headsup settings fragment

src/com/android/settings/SettingsActivity.java
@@ -131,6 +131,7 @@ import com.android.settingslib.drawer.DashboardCategory;
 import com.android.settingslib.drawer.SettingsDrawerActivity;
 import com.android.settingslib.drawer.Tile;
 import com.pure.settings.PureSettings;
+import com.pure.settings.fragments.HeadsUpSettings;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -360,7 +361,8 @@ public class SettingsActivity extends SettingsDrawerActivity
             PureSettings.class.getName(),
             AmbientSettings.class.getName(),
             DisplayRotation.class.getName(),
-            ExpandedDesktopPreferenceFragment.class.getName()
+            ExpandedDesktopPreferenceFragment.class.getName(),
+            HeadsUpSettings.class.getName()
     };
 
 

SiX-ROM/platform_packages_apps_Settings

src/com/android/settings/SettingsActivity.java
@@ -140,6 +140,7 @@ import com.flash.settings.fragments.FlingSettings;
 import com.flash.settings.fragments.NavbarSettings;
 import com.flash.settings.fragments.SmartbarSettings;
 import com.flash.settings.fragments.PulseSettings;
+import com.flash.settings.fragments.HeadsUpSettings;
 
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -381,7 +382,8 @@ public class SettingsActivity extends SettingsDrawerActivity
             NavbarSettings.class.getName(),
             FlingSettings.class.getName(),
             SmartbarSettings.class.getName(),
-            PulseSettings.class.getName()
+            PulseSettings.class.getName(),
+            HeadsUpSettings.class.getName()
     };
 
 