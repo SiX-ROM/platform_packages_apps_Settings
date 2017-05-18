Flash-ROM/packages_apps_Settings

Model Name and ROM Version in About Phone
Change-Id: I2521a2d683312bd41449e7ab9316255c1f602e63

src/com/android/settings/DeviceInfoSettings.java
@@ -63,6 +63,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String KEY_EQUIPMENT_ID = "fcc_equipment_id";
     private static final String PROPERTY_EQUIPMENT_ID = "ro.ril.fccid";
     private static final String KEY_DEVICE_FEEDBACK = "device_feedback";
+    private static final String KEY_ROM_VERSION = "rom_version";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
@@ -111,6 +112,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(DeviceInfoUtils.getFormattedKernelVersion());
+        setValueSummary(KEY_ROM_VERSION, "ro.rom.version");
+        findPreference(KEY_ROM_VERSION).setEnabled(true);
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);

SiX-ROM/platform_packages_apps_Settings

src/com/android/settings/DeviceInfoSettings.java
@@ -63,6 +63,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String KEY_EQUIPMENT_ID = "fcc_equipment_id";
     private static final String PROPERTY_EQUIPMENT_ID = "ro.ril.fccid";
     private static final String KEY_DEVICE_FEEDBACK = "device_feedback";
+    private static final String KEY_FLASH_VERSION = "flash_version";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
@@ -111,6 +112,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(DeviceInfoUtils.getFormattedKernelVersion());
+        setValueSummary(KEY_FLASH_VERSION, "ro.flash.version");
+        findPreference(KEY_FLASH_VERSION).setEnabled(true);
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);